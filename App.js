89275496b413f4aadc55a5df89c4895ba28e0374
Tina Tsai <62583774+tinatsaitp@users.noreply.github.com>
2022-10-31 19:53:25 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,144 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Dimensions, useWindowDimensions} from 'react-native';
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import Card from './src/components/card';
 import users from './assets/data/users';
-import {snapPoint} from "react-native-redash";
-import Animated, {
-  useSharedValue,
-  useDerivedValue,
-  useAnimatedStyle,
-  withSpring,
-  useAnimatedGestureHandler,
-  interpolate,
-  runOnJS,
-} from 'react-native-reanimated';
-import {gestureHandlerRootHOC, PanGestureHandler} from 'react-native-gesture-handler';
+
+import AnimatedStack from './src/components/AnimatedStack';
 
 const App = gestureHandlerRootHOC(() => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [middleCardIndex, setMiddleCardIndex] = useState(currentIndex + 1);
-  const [backCardIndex, setBackCardIndex] = useState(middleCardIndex + 1);
-  const currentProfile = users[currentIndex];
-  const middleCardProfile = users[middleCardIndex];
-  const backCardProfile = users[backCardIndex];
-
-  const {width: screenWidth, height: screenHeight} = useWindowDimensions();
-  const SNAP_POINTS_X = [-screenWidth, 0, screenWidth];
-  const SNAP_POINTS_Y = [-screenHeight, 0, screenHeight];
-  //const hiddenTranslate = 2 * screenWidth;
-
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
-
-  const midRotate = useDerivedValue(() => interpolate(
-    translateX.value,
-    [-screenWidth, 0, screenWidth],
-    [0, 5, 0]) + 'deg',  
-  );
-  const midScale = useDerivedValue(() => interpolate(
-    translateX.value,
-    [-screenWidth, 0, screenWidth],
-    [1, 0.9, 1]),
-  );
-  const backRotate = useDerivedValue(() => interpolate(
-    translateX.value,
-    [-screenWidth, 0, screenWidth],
-    [5, -10, 5]) + 'deg',  
-  );
-  const backScale = useDerivedValue(() => interpolate(
-    translateX.value,
-    [-screenWidth, 0, screenWidth],
-    [0.9, 0.8, 0.9]),
-  );
-
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: (_, context) => {
-      context.startX = translateX.value;
-      context.startY = translateY.value;
-    },
-    onActive: (event, context) => {
-      translateX.value = context.startX + event.translationX;
-      translateY.value = context.startY + event.translationY;
-    },
-    onEnd: ({velocityX, velocityY}) => {
-      const destX = snapPoint(translateX.value, velocityY, SNAP_POINTS_X);
-      const destY = snapPoint(translateY.value, velocityX, SNAP_POINTS_Y);
-      translateX.value = withSpring(destX, {velocity: velocityX});
-      translateY.value = withSpring(destY, {velocity: velocityY});
-      runOnJS(setCurrentIndex)(currentIndex + 1);
-      runOnJS(setMiddleCardIndex)(middleCardIndex + 1);
-      runOnJS(setBackCardIndex)(backCardIndex + 1);
-    },
-  });
-
-  useEffect(() => {
-    translateX.value = 0;
-    translateY.value = 0;
-    setMiddleCardIndex(currentIndex + 1);
-    setBackCardIndex(middleCardIndex + 1);
-  }, [currentIndex, translateX, translateY]
-  );
-
-  const cardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {translateX: translateX.value,},
-      {translateY: translateY.value,},
-    ],
-  }));
-
-  const middleCardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        scale: midScale.value,
-      },
-      {
-        rotate: midRotate.value,
-      },
-    ],
-  }));
-
-  const backCardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        scale: backScale.value,
-      },
-      {
-        rotate: backRotate.value,
-      },
-    ],
-}));
-
-
-
-
-
   return (
     <View style={styles.pageContainer}>
-      {backCardProfile && (
-        <View style={styles.backCard}>
-          <Animated.View style={[styles.animatedCard, backCardStyle]}>
-            <Card user={backCardProfile}/> 
-          </Animated.View> 
-        </View>
-      )}
-      
-      {middleCardProfile && (
-        <View style={styles.middleCard}>
-          <Animated.View style={[styles.animatedCard, middleCardStyle]}>
-            <Card user={middleCardProfile}/> 
-          </Animated.View>        
-        </View>
-      )}
-
-      {currentProfile && (
-        <PanGestureHandler onGestureEvent={gestureHandler}>
-          <Animated.View style={[styles.animatedCard, cardStyle]}>
-            <Card user={currentProfile}/>
-          </Animated.View>
-        </PanGestureHandler>
-      )}
-            
+      <AnimatedStack 
+        data={users}
+        renderItem={(({item}) => <Card user={item} />)}
+      />
       <View style={styles.navbar}>
         <Icon name='cards-diamond' style={styles.navIcon}/>
         <Icon name='cards-club' style={styles.navIcon}/>
@@ -153,29 +28,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     flex: 1,
-  },
-
-  animatedCard: {
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-    marginBottom: -92,
-  },
-
-  middleCard: {
-    position: 'absolute',
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-    marginBottom: -92,
-  },
-
-  backCard: {
-    position: 'absolute',
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-    marginBottom: -92,
+    maxWidth: '100%',
   },
 
   navIcon: {
