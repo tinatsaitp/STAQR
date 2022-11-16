8ae5f5d3e58ad8fff3682cc41841d21528f86468
Tina Tsai <62583774+tinatsaitp@users.noreply.github.com>
2022-11-16 12:35:20 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import Navbar from './src/screens/Navbar';
 
+
+
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <HomeScreen />
-      <Navbar />
+      <LoginScreen />
     </View>
   );
 };
