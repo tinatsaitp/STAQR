9716861ec4d745beb95d5f4ee70bfe0dd2f539c4
Tina Tsai <62583774+tinatsaitp@users.noreply.github.com>
2022-11-20 21:30:26 -0500

    Update App.js

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import {View, StyleSheet} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import Navbar from './src/screens/Navbar';
+import ProfileScreen from './src/screens/ProfileScreen';
+import MenuWindow from './src/screens/MenuWindow';
 
 
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <LoginScreen />
+      <ProfileScreen />
     </View>
   );
 };
