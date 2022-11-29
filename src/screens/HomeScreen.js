b66b4d0b7da71c94bdfc750db64c46a7d3a70f01
Tina Tsai <62583774+tinatsaitp@users.noreply.github.com>
2022-11-29 15:27:06 -0500

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Card from '../components/card';
 import users from '../../assets/data/users';
-
 import AnimatedStack from '../components/AnimatedStack';
 
-const HomeScreen = () => {
+
+
+const HomeScreen = ({navigation}) => {
+
+  const profilePage = () => navigation.navigate('Profile');
+  
   return (
-    <View>
-      <AnimatedStack 
+    <View style={styles.root}>
+      <AnimatedStack
         data={users}
         renderItem={(({item}) => <Card user={item} />)}
       />
+      <View style={styles.navbar}>
+        <Icon name='cards-diamond' style={styles.navIcon}/>
+        <Icon name='cards-club' style={styles.navIcon}/>
+        <Icon name='cards' style={styles.navIcon} onPress={profilePage}/>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    height: '100%',
+  },
+
+  navIcon: {
+    fontSize: 60,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 5,
+    marginBottom: 5,
+  },
+
+  navbar: {
+    position: 'absolute',
+    flexDirection: 'row',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: '#3ec9c4',
+    justifyContent: 'space-evenly',   
+  },
+});
 
 export default HomeScreen;
