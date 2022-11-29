c4b1710be2fc10536fda7d3e8c8bc61e6a63c3b7
Tina Tsai <62583774+tinatsaitp@users.noreply.github.com>
2022-11-29 15:27:13 -0500

    Update ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,42 +1,66 @@
 import React from 'react';
 import {StyleSheet, View, Text, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import MenuWindow from './MenuWindow';
+import {
+    Menu,
+    MenuProvider,
+    MenuOptions,
+    MenuOption,
+    MenuTrigger
+} from 'react-native-popup-menu';
 import Card from '../components/card';
+import users from '../../assets/data/users';
+import ProfileCards from '../components/ProfileCards';
 
-const Profile = () => {
-const image = {uri: "https://cdn.discordapp.com/attachments/1014586144681373738/1042186131443286096/STAQR_Blue_Vary.png"};
-  return(
-    <View style={styles.root}>
-        <View style={styles.iconBar}>
-            <Icon name='chevron-left' style={styles.backIcon}/>
-            <MenuWindow />
-        </View>
+
+const Profile = ({navigation}) => {
+
+    const loginPage = () => navigation.navigate('Login');
+    const homepage = () => navigation.navigate('Home');
+
+    const image = {uri: "https://cdn.discordapp.com/attachments/1014586144681373738/1042186131443286096/STAQR_Blue_Vary.png"};
+    return(
+        <View style={styles.root}>
+            <View style={styles.iconBar}>
+                <Icon name='chevron-left' style={styles.backIcon} onPress={homepage}/>
                 
-        <View style={styles.profileContainer}>
-            <Image
-                source={image}
-                style={styles.image}
-            />
+                <MenuProvider>
+                    <Menu style={styles.settingContainer}>
+                        <MenuTrigger>
+                            <Icon name='dots-horizontal' size = {35} color='black'/>
+                        </MenuTrigger>
+                        <MenuOptions style={styles.settingWindow}>
+                            <MenuOption onSelect={() => alert('Logging Out ...')}>
+                            <Text style={styles.settingWindowLogout} onPress={loginPage}>Log Out</Text>
+                            </MenuOption>
+                        </MenuOptions>
+                    </Menu>
+                </MenuProvider>
+            </View>
+                    
+            <View style={styles.profileContainer}>
+                <Image
+                    source={image}
+                    style={styles.image}
+                />
+                
+                <Text style={styles.username}> STAQR </Text>
             
-            <Text style={styles.username}> STAQR </Text>
-        
-            <View style={styles.bioNumBar}>
-                <Text style={styles.bioNum}> 2 </Text>
-                <Text style={styles.bioNum}> 27 </Text>
+                <View style={styles.bioNumBar}>
+                    <Text style={styles.bioNum}> 2 </Text>
+                    <Text style={styles.bioNum}> 27 </Text>
+                </View>
+                <View style={styles.bioNumBar}>
+                    <Text style={styles.bioNumText}> Liked Posts </Text>
+                    <Text style={styles.bioNumText}> Followers </Text>
+                </View>
             </View>
-            <View style={styles.bioNumBar}>
-                <Text style={styles.bioNumText}> Liked Posts </Text>
-                <Text style={styles.bioNumText}> Followers </Text>
+
+            <View style={{borderWidth: StyleSheet.hairlineWidth, width:'100%',}}></View>
+
+            <View style={styles.contentContainer}>
             </View>
         </View>
-
-        <View style={{borderWidth: StyleSheet.hairlineWidth, width:'100%',}}></View>
-
-        <View style={styles.contentContainer}>
-        </View>
-        
-      </View>
   );
 };
 
@@ -44,7 +68,6 @@ const image = {uri: "https://cdn.discordapp.com/attachments/1014586144681373738/
 const styles = StyleSheet.create({
     root: {
         alignItems: 'center',
-        flex: 1,
         width: '100%',
     },
 
@@ -58,6 +81,29 @@ const styles = StyleSheet.create({
     backIcon: {
         fontSize: 35,
         color: 'black',
+    },
+
+    settingContainer: {
+        marginLeft: 321,
+        //backgroundColor: 'purple',
+    },
+
+    settingWindow: {
+        marginLeft: 97,
+        position: 'absolute',
+        //backgroundColor: 'lightpink',
+    },
+
+    settingWindowText: {
+        color: 'grey',
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+
+    settingWindowLogout: {
+        color: '#3ec9c4',
+        fontWeight: 'bold',
+        fontSize: 15,
     },
 
     profileContainer: {
